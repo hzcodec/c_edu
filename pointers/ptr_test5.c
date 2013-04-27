199713c9a9401c9d5eec84b8c3e4717499bd8339
Heinz Samuelsson <heinz.samuelsson@gmail.com>
2013-04-27 19:00:36 +0200

    ptr_test5 fixed.

--- pointers/ptr_test5.c
+++ pointers/ptr_test5.c
@@ -5,32 +5,52 @@
     Reference   : 
     Description : Test of pointers and structs.
 */
+
 #include <stdio.h>
 
 typedef struct {
     int x;
     int y;
-} RegA[5];
+} RegA;
 
 
 int main() {
 
     // pointer to a struct
-    RegA  rega;
+    RegA  rega[5];
     RegA* p_rega;
 
     // assign address to pointer
-    p_rega = &rega;
-//    rega[0].x = 44;
-    p_rega->x = 99;
-//    p_rega->y = 88;
-//    printf("rega.x: %d\n",rega.x);
-//    printf("rega.y: %d\n",rega.y);
+    p_rega = &rega[0];
+
+    p_rega->x = 11;
+    p_rega->y = 22;
+    printf("rega->x: %d\n",p_rega->x);
+    printf("rega->y: %d\n",p_rega->y);
+
+    // move pointer to next index
+    p_rega++;
+
+    p_rega->x = 33;
+    p_rega->y = 44;
+    printf("rega->x: %d\n",p_rega->x);
+    printf("rega->y: %d\n",p_rega->y);
+
+    // move back pointer to previous index
+    p_rega--;
+    printf("rega->x: %d\n",p_rega->x);
+    printf("rega->y: %d\n",p_rega->y);
 
     return 0;
 }
 
 /*
   Result from run:
+    rega->x: 11
+    rega->y: 22
+    rega->x: 33
+    rega->y: 44
+    rega->x: 11
+    rega->y: 22
 */
 
