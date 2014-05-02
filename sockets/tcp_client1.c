6774bff74f1da697f8dde25b5614da20309ec937
Heinz <heinz@RaspberryRed.(none)>
2014-05-02 22:39:33 +0200

    Sockets updated.

---
+++ sockets/tcp_client1.c
@@ -0,0 +1,65 @@
+/*
+    Auther      : Heinz Samuelsson
+    Date        : 2014-05-02
+    File        : udp_client1.c
+    Reference   : Programming with sockets. Page 20.
+
+                  The program creates a socket and initiates a 
+		  connection with the socket given in the command line.
+		  Some data are sent over the connection and then the
+		  socket is closed, ending the connection.
+                  The form of the command line is:
+                      > ./client 127.0.0.1 42695
+*/
+#include <stdio.h>
+#include <stdlib.h>      // exit()
+#include <string.h>      // strlen
+#include <sys/socket.h>
+#include <arpa/inet.h>   // inet_addr
+#include <sys/types.h>
+#include <netdb.h>
+ 
+#define DATA "Jennie Samuelsson, Brunstorps Hage 55"
+
+
+int main(int argc , char *argv[]) {
+
+    int    sock;
+    struct sockaddr_in server;
+    struct hostent *hp;
+    char   buf[1024];
+ 
+    // create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1) {
+        perror("Opening stream socket");
+        exit(1);
+    }
+         
+    // connect socket using name specified by command line
+    server.sin_family = AF_INET;
+    hp = gethostbyname(argv[1]);
+    printf("hp %s\n",hp->h_name);
+ 
+    // gethostbyname() returns a structure including the network
+    // address of the specified host
+    if (hp == (struct hostent*)0) {
+        fprintf(stderr,"%s: unknown host\n",argv[1]);
+        exit(2);
+    }
+     
+    memcpy((char*)&server.sin_addr, (char*)hp->h_addr, hp->h_length);
+    server.sin_port = htons(atoi(argv[2]));
+
+    if (connect(sock,(struct sockaddr*)&server, sizeof server)== -1) {
+        perror("Connecting stream socket");
+        exit(1);
+    }
+
+    if (write(sock,DATA,sizeof DATA) == -1)
+        perror("Writing on stream socket"); 
+ 
+    close(sock);
+     
+    return 0;
+}
---
+++ sockets/tcp_server1.c
@@ -0,0 +1,86 @@
+/*
+    Auther      : Heinz Samuelsson
+    Date        : 2014-05-02
+    File        : udp_client1.c
+    Reference   : Programming with sockets. Page 20.
+                  The program creates a socket and then begins an
+		  infinite loop. Each time through the loop it 
+		  accepts a connection and prints data from it.
+		  When the connection breaks, or the client closes
+                  the connection, the program accepts a new connection.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>       // exit()
+#include <sys/socket.h>
+#include <arpa/inet.h>    // inet_addr
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>       // strlen
+
+#define TRUE 1
+
+
+int main(int argc , char *argv[]) {
+
+    int    sock;
+    int    length;
+    struct sockaddr_in server;
+    int    msgsock;
+    char   buf[1024];
+    int    rval;
+ 
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1) {
+        perror("Opening stream socket");
+        exit(1);
+    }
+         
+    // Bind socket using wildcards
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr =INADDR_ANY;
+    server.sin_port = 0;
+ 
+    if (bind(sock, (struct sockaddr*)&server,sizeof server) == -1) {
+        perror("bindingstream socket");
+        exit(1);
+    }
+     
+    // Find out assigned port number and print it out
+    length = sizeof server;
+    if (getsockname(sock,(struct sockaddr*)&server,&length) == -1) {
+        perror("Getting socket name");
+        exit(1);
+    }
+
+    // network host to short
+    printf("Socket port #%d\n",ntohs(server.sin_port));
+
+    // Start accepting connections
+    listen(sock,5);
+
+    do {
+        msgsock = accept(sock,(struct sockaddr*)0,(int*)0);
+
+        if (msgsock == -1)
+            perror("accept");
+
+        else do {
+            memset(buf, 0, sizeof buf);
+
+            if ((rval = read(msgsock, buf, 1024)) == -1)
+                perror("Reading stream message");
+            if (rval == 0)
+                printf("Ending connection\n");
+            else
+                printf("-->%s\n", buf);
+
+         } while (rval != 0);
+
+         close(msgsock);
+
+     } while (TRUE);
+  
+    return 0;
+}
